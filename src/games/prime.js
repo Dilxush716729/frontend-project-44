8825d425593dc9777b48399f0877f6f3e4a031ed
Cuppuchino228 <okakentok540@gmail.com>
2026-04-03 04:58:04 +0700

    feat: implement gcd, progression and prime games

---
+++ src/games/prime.js
@@ -0,0 +1,20 @@
+import getRandomInt from '../utils.js';
+import runGame from '../index.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const generateRound = () => {
+  const question = getRandomInt(1, 100);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return [String(question), correctAnswer];
+};
+
+export default () => runGame(description, generateRound);
